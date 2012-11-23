bc6e350083534e446072539b3a9575c2cd05b313
Nathan Collins <nathan.collins@gmail.com>
2012-11-22 23:30:40 -0800

    Make main.py solve part 1 and part of part 2; add servers in servers.txt.
    The "part of part 2" is connection counts.  Lots of cruft remains in
    main.py that I don't need and could be removed ... this diff just has
    my additions.

--- project-2/collins/main.py
+++ project-2/collins/main.py
@@ -63,6 +63,16 @@ class Connection:
         return (hash(self.p1[0]) ^ hash(self.p1[1])
                 ^ hash(self.p2[0]) ^ hash(self.p2[1]))
 
+# Copied from C pcap tutorial. These must be def'd by Impacket
+# somewhere ...
+TH_FIN = 0x01
+TH_SYN = 0x02
+TH_RST = 0x04
+TH_PUSH = 0x08
+TH_ACK = 0x10
+TH_URG = 0x20
+TH_ECE = 0x40
+TH_CWR = 0x80
 
 class Decoder:
     def __init__(self, pcapObj):
@@ -76,7 +86,8 @@ class Decoder:
             raise Exception("Datalink type not supported: " % datalink)
 
         self.pcap = pcapObj
-        self.connections = {}
+        self.handshakes = {}
+        self.connections = []
 
     def start(self):
         # Sniff ad infinitum.
@@ -84,6 +95,47 @@ class Decoder:
         self.pcap.loop(0, self.packetHandler)
 
     def packetHandler(self, hdr, data):
+        p = self.decoder.decode(data)
+        ip = p.child()
+        tcp = ip.child()
+        src = (ip.get_ip_src(), tcp.get_th_sport() )
+        dst = (ip.get_ip_dst(), tcp.get_th_dport() )
+
+        # Handshake 1
+        if tcp.get_th_flags() == TH_SYN:
+            #print "1"
+            client_server = (src,dst)
+            self.handshakes[client_server] = { "client_seq" : tcp.get_th_seq() }
+        # Handshake 2
+        #
+        # XXX: For the purposes of defining a "server", replying with
+        # SYN-ACK may be sufficient.  In particular, a port scan would
+        # identify servers that I don't identify, since I'm requiring
+        # the handshake to complete.
+        elif tcp.get_th_flags() == TH_SYN | TH_ACK:
+            #print "2?"
+            client_server = (dst,src)
+            if client_server in self.handshakes:
+                #print "2??"
+                hs = self.handshakes[client_server]
+                if hs.get("client_seq",None) + 1 == tcp.get_th_ack():
+                    hs["server_seq"] = tcp.get_th_seq()
+                else:
+                    #print "2 fail: expecting %i, got $i" % (hs.get("client_seq",None),tcp.get_th_ack())
+                    # Could be more paranoid here: got an unexpected handshake 2.
+                    pass
+        # Handshake 3
+        elif tcp.get_th_flags() == TH_ACK:
+            #print "3"
+            client_server = (src,dst)
+            if client_server in self.handshakes:
+                hs = self.handshakes[client_server]
+                if hs.get("client_seq",None) + 1 == tcp.get_th_seq() and \
+                   hs.get("server_seq",None) + 1 == tcp.get_th_ack():
+                    self.handshakes.pop(client_server)
+                    self.connections.append(client_server)
+
+    def packetHandler_(self, hdr, data):
         """Handles an incoming pcap packet. This method only knows how
         to recognize TCP/IP connections.
         Be sure that only TCP packets are passed onto this handler (or
@@ -129,10 +181,24 @@ def main(filename):
     # At the moment the callback only accepts TCP/IP packets.
     p.setfilter(r'ip proto \tcp')
 
-    print "Reading from %s: linktype=%d" % (filename, p.datalink())
+    #print "Reading from %s: linktype=%d" % (filename, p.datalink())
 
     # Start decoding process.
-    Decoder(p).start()
+    global d
+    d = Decoder(p)
+    d.start()
+
+    servers = [ s for (_,s) in d.connections ]
+    server_stats = {}
+    for s in servers:
+        server_stats[s] = server_stats.get(s,0) + 1
+    rekeyed_server_stats = [(c, "%s:%i" % (s,p)) for ((s,p),c) in server_stats.iteritems()]
+    sorted_server_stats = sorted(rekeyed_server_stats, reverse=True)
+    print "Connections Server"
+    print "=========== ======"
+    print
+    for (c,s) in sorted_server_stats:
+        print "%-11i %s" % (c,s)
 
 
 # Process command-line arguments.
