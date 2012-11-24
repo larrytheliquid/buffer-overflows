e07faa5ccd081d56299d7915f19db5d8a46ba075
Nathan Collins <nathan.collins@gmail.com>
2012-11-23 17:08:33 -0800

    Store and build and install libs in ./lib.

--- project-2/collins/local.py
+++ project-2/collins/local.py
@@ -1,4 +1,4 @@
 # Add our locally installed libs to the search path.
 import sys
-sys.path.append('local/lib/python%s.%s/site-packages'
+sys.path.append('lib/local/lib/python%s.%s/site-packages'
                 % (sys.version_info.major, sys.version_info.minor))
